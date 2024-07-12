ccd970cee90c8cd988c8eaecef112fcb1197d650
Jyoshna-15 <117263367+Jyoshna-15@users.noreply.github.com>
2024-07-12 15:58:56 +0530

    Add files via upload

---
+++ Strings.js
@@ -0,0 +1,40 @@
+// console.log(typeof "It's raining ");
+// let word="raining";
+
+// console.log(word.toLocaleLowerCase());
+// let number="raining";
+// let string="string is a name";
+// let String=10;
+// console.log(string);
+// console.log(String);
+
+
+// console.log("hello and good evening".length);
+// console.log("hello".charAt(4));
+
+
+// console.log(typeof true);
+// console.lof(typeof false);
+// console.log( isNaN('five'));
+
+// let condition=10/'hello';
+// if(condition){
+//     console.log("very true")
+
+// }else{
+//     console,log("Very wrong")
+// }
+// console.log('hello'.includes('e'))
+
+
+let score;
+let result=null;
+let status=null;
+if(score){
+        console.log("very true")
+    
+    }else{
+        console,log("Very wrong")
+    }
+// console.log(typeof score);
+// console.log(typeof result);
