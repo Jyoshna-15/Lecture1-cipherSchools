60a285bd57d56ea6631e3c6cbd177d3e624128d1
Jyoshna-15 <117263367+Jyoshna-15@users.noreply.github.com>
2024-07-12 15:42:16 +0530

    Add files via upload

---
+++ exapmle.js
@@ -0,0 +1,36 @@
+// console.log("JavaScript is awesome");
+// var score=10;
+// let result=50;
+// const PI=3.14;
+// score=15;
+// result=55;
+// PI=3,26
+// console.log(score);
+// console.log(result);
+// console.log(PI)
+
+// Datatypes
+// 1.Number
+// 2.Strings
+// 3.Boolean
+// 4.Null
+// 5.Undefined
+// 6.Objects
+// 7.Symbols
+
+let score=10;
+let result="10";
+let final=score+result;
+let penalty=score-result;
+let totalGames=5;
+let avg=final/totalGames;
+
+console.log(score,result);
+console.log(final);
+console.log(final2);
+
+console.log(typeof(final));
+console.log(avg);
+console.log(typeof avg);
+let value= (final**2)*10/(penalty+totalGames);
+console.log(value);
